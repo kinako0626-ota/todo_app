03da0dd77c954466e034ccc1f2dd466dd1b34905
Ryunosuke.Ota <kr07031321@gmail.com>
2024-05-21 00:16:47 +0900

    add : createとread処理追加

--- todo_api/main.go
+++ todo_api/main.go
@@ -3,40 +3,60 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 	"github.com/kinako0626-ota/todo_api/src/models"
 	"github.com/kinako0626-ota/todo_api/src/utils"
 )
 
 func main() {
-	// env取得
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file", err)
-	}
-	root := os.Getenv("MYSQL_ROOT_USER")
-	password := os.Getenv("MYSQL_ROOT_PASSWORD")
-	dbname := os.Getenv("MYSQL_DATABASE")
-	db, error := utils.SetupDB(root, password, dbname)
+	db, error := utils.SetupDB()
 	if error != nil {
 		log.Fatalf("Error loading .env file", error)
 	}
-	// マイグレーション
 	db.AutoMigrate(&models.Todo{})
 	r := gin.Default()
 	// GET /todos
 	r.GET("/todos", func(ctx *gin.Context) {
+		// TODO: 一覧取得処理を実装する
+		todos  := []models.Todo{}
+		if error := db.Find(&todos).Error; error != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": error,
+			})
+			return
+		}
+		for i, todo := range todos {
+			log.Println(i, todo)
+		}
+
 		ctx.JSON(http.StatusOK, gin.H{
-			"message": "Hello World!",
+			"todos": todos,
 		})
 	})
 	// POST /todo
 	r.POST("/todo", func(ctx *gin.Context) {
+		// リクエストボディを取得
+		var todo models.Todo
+	
+
+		if error := ctx.BindJSON(&todo); error != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": error,
+			})
+			return
+		}
+		// dbに保存
+		if error := db.Create(&todo).Error; error != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": error,
+			})
+			return
+		}
 		ctx.JSON(http.StatusOK, gin.H{
-			"message": "Hello World!",
+			"status_code": http.StatusOK,
+			"message": "add todo",
+			"todo": todo,
 		})
 	})
 	// PUT /todo/:id
--- todo_api/src/utils/db-setup.go
+++ todo_api/src/utils/db-setup.go
@@ -3,22 +3,29 @@ package utils
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
-func SetupDB(root, password, dbname string) (*gorm.DB ,error) {
+func SetupDB() (*gorm.DB ,error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file", err)
+	}
+	root := os.Getenv("MYSQL_ROOT_USER")
+	password := os.Getenv("MYSQL_ROOT_PASSWORD")
+	dbName := os.Getenv("MYSQL_DATABASE")
 
 	// DB接続
-	dsn := fmt.Sprintf("%s:%s@tcp(mysql_todo_db)/%s?charset=utf8mb4&parseTime=True&loc=Local", root, password, dbname)
+	dsn := fmt.Sprintf("%s:%s@tcp(mysql_todo_db)/%s?charset=utf8mb4&parseTime=True&loc=Local", root, password, dbName)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
-	if err != nil {
-		log.Fatal(err)
-	}
+	
 
 	return db, err
 }
